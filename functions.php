ba1f75d3d65ed0e2bb275c9a1d35a9058fffdbf1
Kostas Nicolacopoulos <knicolacopoulos@gmail.com>
2014-10-22 13:40:08 -0700

    Cleaned functions.php header

--- functions.php
+++ functions.php
@@ -1,12 +1,4 @@
 <?php
-/*
-Author: Eddie Machado
-URL: htp://themble.com/bones/
-
-This is where you can drop your custom functions or
-just edit things like thumbnail sizes, header images,
-sidebars, comments, ect.
-*/
 
 // LOAD BONES CORE (if you remove this, the theme will break)
 require_once( 'library/bones.php' );
