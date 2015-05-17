4de53c93f8152a83c67560248bf846485849cf67
Kostas Nicolacopoulos <knicolacopoulos@gmail.com>
2015-05-16 19:42:50 -0700

    Add conditional comments

--- post-formats/format.php
+++ post-formats/format.php
@@ -44,6 +44,11 @@
 
 			</footer> <?php // end article footer ?>
 
-			<?php //comments_template(); ?>
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			?>
 
 		</article> <?php // end article ?>
