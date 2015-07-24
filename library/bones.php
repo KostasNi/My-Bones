ad99bcfb944742a27e2bd123053ad22577420470
Kostas Nicolacopoulos <knicolacopoulos@gmail.com>
2015-07-23 22:54:54 -0700

    Better Read more links

--- library/bones.php
+++ library/bones.php
@@ -217,7 +217,7 @@ function bones_filter_ptags_on_images( $content ) {
 function bones_excerpt_more() {
 	global $post;
 	// edit here if you like
-	return '...  <a class="excerpt-read-more" href="'. get_permalink( $post->ID ) . '" title="'. __( 'Read ', 'bonestheme' ) . esc_attr( get_the_title( $post->ID ) ).'">'. __( 'Read more &raquo;', 'bonestheme' ) .'</a>';
+	return '...  <a class="excerpt-read-more" href="'. get_permalink( $post->ID ) . '" title="'. __( 'Read ', 'bonestheme' ) . esc_attr( get_the_title( $post->ID ) ).'">'. __( 'Read more &raquo;', 'bonestheme' ) .'<span class="screen-reader-text"> of "'.get_the_title( $post->ID ).'</span></a>';
 }
 
 ?>
